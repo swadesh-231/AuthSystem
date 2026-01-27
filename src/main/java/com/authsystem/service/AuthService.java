9748138ab2ca29b1589a4e25fd8d293309b90dc4
Swadesh Chatterjee <swadeshchatterjee512@gmail.com>
2026-01-27 18:08:07 +0530

    starting with spirng security

--- src/main/java/com/authsystem/service/AuthService.java
+++
@@ -1,8 +0,0 @@
-package com.authsystem.service;
-
-import com.authsystem.dto.SignUpRequest;
-import com.authsystem.dto.UserDto;
-
-public interface AuthService {
-    UserDto registerUser(SignUpRequest signUpRequest);
-}
